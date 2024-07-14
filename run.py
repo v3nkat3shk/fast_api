35f29d40cba0a7b5ee79a1421e72bb5ac1c03f1d
v3nkat3shk <venakteshkumbhar5700@gmail.com>
2024-07-14 09:26:48 +0530

    Docker setup.

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import uvicorn
 if __name__ == '__main__':
     uvicorn.run(
         "server.main:application",
+        host='0.0.0.0',
         port=8080,
         log_level="info",
         reload=True
